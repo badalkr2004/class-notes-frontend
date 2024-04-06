b5687ab48e9086e709a33dda80e1494d3d12bdcf
badalkr2004 <badalkr279@gmail.com>
2024-04-07 03:51:19 +0530

    initialised the frontend and implemented frontend api

---
+++ script.js
@@ -0,0 +1,37 @@
+function uploadImage() {
+    const fileInput = document.getElementById('fileInput');
+    const subIn = document.getElementById("subInput");
+    const preview = document.getElementById('preview');
+  
+    const file = fileInput.files[0];
+
+    
+
+    if (file && subIn.value) {
+      const formData = new FormData();
+      formData.append('file', file);
+      formData.append('subject',subIn.value);
+     
+  
+      fetch('https://class-notes-sharer.vercel.app/api/upload', {
+        method: 'POST',
+        body: formData
+      })
+      .then(response => response.json())
+      .then(data => {
+        // Display uploaded image
+        console.log(data)
+        const imageUrl = data.data.image.url;
+        const img = document.createElement('img');
+        img.src = imageUrl;
+        preview.appendChild(img);
+      })
+      .catch(error => console.error('Error:', error));
+    } else {
+      alert('Please select an image to upload.');
+    }
+  }
+
+
+
+
