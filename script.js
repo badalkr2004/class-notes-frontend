0acc56c47436abd0d64bbb28d77674f5169c2a5d
badalkr2004 <badalkr279@gmail.com>
2024-04-07 15:32:34 +0530

    added subject options in upload.html and some api fetching issues resolved

--- script.js
+++ script.js
@@ -8,31 +8,42 @@ function uploadImage() {
 
     subBtn.disabled = true;
 
-    if (file && subIn.value &&  ((file.size/1024)/1024).toFixed(2) < 10) {
-      const formData = new FormData();
-      formData.append('file', file);
-      formData.append('subject',subIn.value);
+    if (file) {
+      if(subIn.value){
+        if(((file.size/1024)/1024).toFixed(2) < 5){
+          const formData = new FormData();
+          formData.append('file', file);
+          formData.append('subject',subIn.value);
+         
+            
+          fetch('https://class-notes-sharer.vercel.app/api/upload', {
+            method: 'POST',
+            body: formData
+          })
+          .then(response => response.json())
+          .then(data => {
+            // Display uploaded image
+            const imageUrl = data.data.image.url;
+            const img = document.createElement('img');
+            img.src = imageUrl;
+            preview.appendChild(img);
+            subBtn.disabled = false;
+          })
+          .catch(error => {
+            subBtn.disabled = false;
+            console.error('Error:', error)}
+            );
+        }else{
+          alert("File Size should be less than 10 MB.")
+          subBtn.disabled = false;
+        }
+      }else{
+        subBtn.disabled = false;
+        alert("Select subject")
+      }
      
-        
-      fetch('https://class-notes-sharer.vercel.app/api/upload', {
-        method: 'POST',
-        body: formData
-      })
-      .then(response => response.json())
-      .then(data => {
-        // Display uploaded image
-        console.log(data)
-        const imageUrl = data.data.image.url;
-        const img = document.createElement('img');
-        img.src = imageUrl;
-        preview.appendChild(img);
-        subBtn.disabled = false;
-      })
-      .catch(error => {
-        subBtn.disabled = false;
-        console.error('Error:', error)}
-        );
     } else {
+      subBtn.disabled = false;
       alert('Please select an image to upload.');
     }
   }
